3f087261f94c8fb5181b199bbdb6cec34b925d42
lakshmi <jorigelakshmi9@gmail.com>
2025-06-18 14:44:17 +0530

    Add files via upload

---
+++ stuData.java
@@ -0,0 +1,18 @@
+class student{
+String name="Lakshmi";
+int roll=250;
+String branch="EEE";
+float cgpa=7.35f;
+}
+class Employee{
+String name="Ankitha";
+String role=" python developer";
+float salary=60000;
+}
+class stuData{
+public static  void main(String[] args){
+Employee e=new Employee();
+System.out.println("Name:"+e.name);
+}
+}
+ 
